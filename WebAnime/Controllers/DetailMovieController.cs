101674bc89c4591d190984624ee97c01a530a219
LeeDuck <ducprime3172003@gmail.com>
2023-11-15 21:46:40 +0700

    upload template

---
+++ WebAnime/Controllers/DetailMovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAnime.Controllers
+{
+    public class DetailMovieController : Controller
+    {
+        // GET: DetailMovie
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Watch()
+        {
+            return View();
+        }
+    }
+}
